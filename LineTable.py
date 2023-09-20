15cb3e3736564282876c2725bd283c2a11f445e9
york8536 <york8536@gmail.com>
2023-09-20 17:47:06 +0800

    20230920
    尋找單一連線功能

--- LineTable.py
+++ LineTable.py
@@ -51,6 +51,7 @@ lineTable =  [
         [3, 2, 1, 3, 3]
     ]
 #-----------------------------------------------------------------以下為篩選無重複賠付線
+result=[]
 def has_duplicate_first_three(arr, arr_list):
     # 取得 arr 的前三個元素
     first_three = arr[:3]
@@ -65,21 +66,30 @@ def has_duplicate_first_three(arr, arr_list):
 result = [arr for arr in lineTable if not has_duplicate_first_three(arr, lineTable)]
 
 print(result)
+
 # -----------------------------------------------------------------以下為索引編號
 
 # target_line = [1, 2, 1, 2, 1] # 輸入索引的賠付線 EX:[1, 2, 1, 2, 1]
-index = None  # 存放結果的欄位
-
-for x in result:
-    target_line = x
-    for i, line in enumerate(lineTable):
-        if line == target_line:
-            index = i + 1  
-            break
- 
-    if index is not None:
-        print(f"第{index}線.")
-    else:
-        print("找不到")
+def findLine():
+    index = None  # 存放結果的欄位
+    for x in result:
+        target_line = x
+        for i, line in enumerate(lineTable):
+            if line == target_line:
+                index = i + 1  
+                break
+    
+        if index is not None:
+            print(f"第{index}線.")
+        else:
+            print("找不到")
         
 # -----------------------------------------------------------------
+def addscript(script):
+    for x in result:
+        for y in range(5):
+            script[y]=script[y]+x[y]
+        print(script)
+
+# findLine()
+addscript([1,1,1,1,1])
